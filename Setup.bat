79ee5f59213803508cb17b6829429f2d21ef0c73
By2048 <1247079575@qq.com>
2020-10-16 00:14:20 +0800

    add echo

--- Setup.bat
+++ Setup.bat
@@ -1,11 +1,12 @@
 @echo off
 @Title Start Ahk
 
-mode 40,20
+mode 30,20
 cd /d %~dp0
 
-start .\Setup.ahk
 
+
+start .\Setup.ahk
 start .\Keys\All.ahk
 start .\Keys\AppsKey.ahk
 start .\Keys\Capslock.ahk
@@ -14,7 +15,6 @@ start .\Keys\LWin.ahk
 start .\Keys\RAlt.ahk
 start .\Keys\RShift.ahk
 start .\Keys\RWin.ahk
-
 Start .\Other\Dexpot.ahk
 Start .\Other\Input.ahk
 start .\Other\Keyboard.ahk
@@ -22,15 +22,35 @@ start .\Other\Loop.ahk
 start .\Other\Software.ahk
 start .\Other\Windows.ahk
 
+echo.
+echo start .\Setup.ahk
+echo start .\Keys\All.ahk
+echo start .\Keys\AppsKey.ahk
+echo start .\Keys\Capslock.ahk
+echo start .\Keys\LAlt.ahk
+echo start .\Keys\LWin.ahk
+echo start .\Keys\RAlt.ahk
+echo start .\Keys\RShift.ahk
+echo start .\Keys\RWin.ahk
+echo start .\Other\Dexpot.ahk
+echo start .\Other\Input.ahk
+echo start .\Other\Keyboard.ahk
+echo start .\Other\Loop.ahk
+echo start .\Other\Software.ahk
+echo start .\Other\Windows.ahk
+
+
 
 if exist  %~dp0Other\Private.ahk (
     start .\Other\Private.ahk
+    echo start .\Other\Private.ahk
 )
 if exist %~dp0Test.ahk (
     start .\Test.ahk
+    echo start .\Test.ahk
 )
 
-echo  --------------------------
+
 
 ping -n 2 127.0.0.1 > nul
 
