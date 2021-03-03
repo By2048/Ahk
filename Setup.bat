b0ba3168aa93fb9187821d40a65944c75a62159e
By2048 <1247079575@qq.com>
2021-03-04 00:22:08 +0800

    update

--- Setup.bat
+++ Setup.bat
@@ -14,37 +14,37 @@ if "%command%"=="" (
 
 if %command%==start (
     echo.
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Setup.ahk      
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Setup.ahk    
     @REM =========================================================
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\All.ahk      
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\All.ahk      start
     echo   start .\Keys\All.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\Capslock.ahk 
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\Capslock.ahk start
     echo   start .\Keys\Capslock.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\LWin.ahk     
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\LWin.ahk     start
     echo   start .\Keys\LWin.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RAlt.ahk     
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RAlt.ahk     start
     echo   start .\Keys\RAlt.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RCtrl.ahk    
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RCtrl.ahk    start
     echo   start .\Keys\RCtrl.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RShift.ahk   
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RShift.ahk   start
     echo   start .\Keys\RShift.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RWin.ahk     
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RWin.ahk     start
     echo   start .\Keys\RWin.ahk
     @REM =========================================================
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Software\All.ahk  
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Software\All.ahk  start
     echo   start .\Software\All.ahk
     @REM =========================================================
-    Start  D:\AutoHotkey\AutoHotkey.exe  .\Input.ahk         
+    Start  D:\AutoHotkey\AutoHotkey.exe  .\Input.ahk         start
     echo   start .\Input.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Loop.ahk          
+    start  D:\AutoHotkey\AutoHotkey.exe  .\Loop.ahk          start
     echo   start .\Loop.ahk
     @REM =========================================================
     if exist %~dp0Private.ahk (
-        start  D:\AutoHotkey\AutoHotkey.exe  .\Private.ahk   
+        start  D:\AutoHotkey\AutoHotkey.exe  .\Private.ahk   start
         echo   start .\Private.ahk
     )
     if exist %~dp0Test.ahk (
-        start  D:\AutoHotkey\AutoHotkey.exe  .\Test.ahk      
+        start  D:\AutoHotkey\AutoHotkey.exe  .\Test.ahk      start
         echo   start .\Test.ahk
     )
     echo.
--- Tool/Base.ahk
+++ Tool/Base.ahk
@@ -1,6 +1,10 @@
 
 for Arg_Index, Arg_Value in A_Args {
-    if (Arg_Value="stop") {
+    if (Arg_Value="start" or Arg_Value="reload") {
+        Reload
+    } else if (Arg_Value="stop" or Arg_Value="exit") {
         ExitApp
+    } else if (Arg_Value="pause") {
+        Pause
     }
 }
