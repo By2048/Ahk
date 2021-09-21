02544da53d337c6ee06afe650dd42c5bd72a5951
By2048 <1247079575@qq.com>
2021-09-21 20:17:41 +0800

    update

--- Setup.bat
+++ Setup.bat
@@ -1,10 +1,33 @@
-﻿
+
 @title [Start Ahk]
 @echo off
 
 mode 40,25
 
-cd /d %~dp0
+set AHK=D:\AutoHotkey\AutoHotkey.exe
+
+set folder=%~dp0
+set folder=%folder:\\=\%
+
+set test_ahk=%folder%\Test.ahk
+set test_ahk=%test_ahk:\\=\%
+
+set private_ahk=%folder%\Private.ahk
+set private_ahk=%private_ahk:\\=\%
+
+set ignore_lol_ahk=%folder%\Software\LOL.Ignore.ahk
+set ignore_lol_ahk=%ignore_lol_ahk:\\=\%
+
+cd /d %folder%
+
+if not exist %private_ahk% (
+    echo Create %private_ahk%
+    echo ;Ignore File; > %private_ahk%
+)
+if not exist %ignore_lol_ahk% (
+    echo Create %ignore_lol_ahk%
+    echo ;Ignore File; > %ignore_lol_ahk%
+)
 
 set command=%1%
 
@@ -14,80 +37,80 @@ if "%command%"=="" (
 
 if %command%==start (
     echo.
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Setup.ahk    
+    start %AHK% .\Setup.ahk    
     @REM =========================================================
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\All.ahk      start
-    echo   start .\Keys\All.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\Capslock.ahk start
-    echo   start .\Keys\Capslock.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\LWin.ahk     start
-    echo   start .\Keys\LWin.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RAlt.ahk     start
-    echo   start .\Keys\RAlt.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RShift.ahk   start
-    echo   start .\Keys\RShift.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RWin.ahk     start
-    echo   start .\Keys\RWin.ahk
+    start %AHK% .\Keys\All.ahk      start
+    echo  start .\Keys\All.ahk
+    start %AHK% .\Keys\Capslock.ahk start
+    echo  start .\Keys\Capslock.ahk
+    start %AHK% .\Keys\LWin.ahk     start
+    echo  start .\Keys\LWin.ahk
+    start %AHK% .\Keys\RAlt.ahk     start
+    echo  start .\Keys\RAlt.ahk
+    start %AHK% .\Keys\RShift.ahk   start
+    echo  start .\Keys\RShift.ahk
+    start %AHK% .\Keys\RWin.ahk     start
+    echo  start .\Keys\RWin.ahk
     @REM =========================================================
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Software\All.ahk  start
-    echo   start .\Software\All.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RCtrl.ahk    start
-    echo   start .\Keys\RCtrl.ahk
+    start %AHK% .\Software\All.ahk  start
+    echo  start .\Software\All.ahk
+    start %AHK% .\Keys\RCtrl.ahk    start
+    echo  start .\Keys\RCtrl.ahk
     @REM =========================================================
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Other\Dexpot.ahk  start
-    echo   start .\Other\Dexpot.ahk
+    start %AHK% .\Other\Dexpot.ahk  start
+    echo  start .\Other\Dexpot.ahk
     @REM =========================================================
-    Start  D:\AutoHotkey\AutoHotkey.exe  .\Input.ahk         start
-    echo   start .\Input.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Loop.ahk          start
-    echo   start .\Loop.ahk
+    Start %AHK% .\Input.ahk         start
+    echo  start .\Input.ahk
+    start %AHK% .\Loop.ahk          start
+    echo  start .\Loop.ahk
     @REM =========================================================
-    if exist %~dp0Private.ahk (
-        start  D:\AutoHotkey\AutoHotkey.exe  .\Private.ahk   start
-        echo   start .\Private.ahk
+    if exist %private_ahk% (
+        start %AHK% .\Private.ahk   start
+        echo  start .\Private.ahk
     )
-    if exist %~dp0Test.ahk (
-        start  D:\AutoHotkey\AutoHotkey.exe  .\Test.ahk      start
-        echo   start .\Test.ahk
+    if exist %test_ahk% (
+        start %AHK% .\Test.ahk      start
+        echo  start .\Test.ahk
     )
     echo.
 )
 
 if %command%==stop (
     echo.
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\All.ahk       stop
-    echo   stop .\Keys\All.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\Capslock.ahk  stop
-    echo   stop .\Keys\Capslock.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\LWin.ahk      stop
-    echo   stop .\Keys\LWin.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RAlt.ahk      stop
-    echo   stop .\Keys\RAlt.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RShift.ahk    stop
-    echo   stop .\Keys\RShift.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RWin.ahk      stop
-    echo   stop .\Keys\RWin.ahk
+    start %AHK% .\Keys\All.ahk      stop
+    echo  stop  .\Keys\All.ahk
+    start %AHK% .\Keys\Capslock.ahk stop
+    echo  stop  .\Keys\Capslock.ahk
+    start %AHK% .\Keys\LWin.ahk     stop
+    echo  stop  .\Keys\LWin.ahk
+    start %AHK% .\Keys\RAlt.ahk     stop
+    echo  stop  .\Keys\RAlt.ahk
+    start %AHK% .\Keys\RShift.ahk   stop
+    echo  stop  .\Keys\RShift.ahk
+    start %AHK% .\Keys\RWin.ahk     stop
+    echo  stop  .\Keys\RWin.ahk
     @REM =========================================================    
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Software\All.ahk   stop
-    echo   stop .\Software\All.ahk  
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Keys\RCtrl.ahk     stop
-    echo   stop .\Keys\RCtrl.ahk
+    start %AHK% .\Software\All.ahk  stop
+    echo  stop  .\Software\All.ahk  
+    start %AHK% .\Keys\RCtrl.ahk    stop
+    echo  stop  .\Keys\RCtrl.ahk
     @REM =========================================================    
-    start  D:\AutoHotkey\AutoHotkey.exe  .\Other\Dexpot.ahk   stop
-    echo   start .\Other\Dexpot.ahk
+    start %AHK% .\Other\Dexpot.ahk  stop
+    echo  stop  .\Other\Dexpot.ahk
     @REM =========================================================
-    Start  D:\AutoHotkey\AutoHotkey.exe .\Input.ahk           stop
-    echo   stop .\Input.ahk
-    start  D:\AutoHotkey\AutoHotkey.exe .\Loop.ahk            stop
-    echo   stop .\Loop.ahk
+    Start %AHK% .\Input.ahk         stop
+    echo  stop  .\Input.ahk
+    start %AHK% .\Loop.ahk          stop
+    echo  stop  .\Loop.ahk
     @REM =========================================================
-    if exist %~dp0Private.ahk (
-        start  D:\AutoHotkey\AutoHotkey.exe  .\Private.ahk    stop
-        echo   stop .\Private.ahk
+    if exist %private_ahk% (
+        start %AHK% .\Private.ahk   stop
+        echo  stop  .\Private.ahk
     )
-    if exist %~dp0Test.ahk (
-        start  D:\AutoHotkey\AutoHotkey.exe  .\Test.ahk       stop
-        echo   stop .\Test.ahk
+    if exist %test_ahk% (
+        start %AHK% .\Test.ahk      stop
+        echo  stop  .\Test.ahk
     )
     echo.
 ) 
