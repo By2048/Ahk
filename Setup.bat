9698ec0a0c6f2cffceedc8fdc160a221d4189143
By2048 <1247079575@qq.com>
2022-06-20 12:23:36 +0800

    启动过程优化

--- Setup.bat
+++ Setup.bat
@@ -11,37 +11,45 @@ Echo  =========== %CD% ===========
 
 SetLocal
 
-@REM ======================================================================
+::#region Init Ahk Files
+Set       Folder=%~dp0
+Set   Config.ahk=.\Config\Base.ahk
+Set     Init.ahk=.\Other\Init.ahk
+Set    Setup.ahk=.\Other\Setup.ahk
+Set   Screen.ahk=.\Tool\Screen.ahk
+Set     Keys.ahk=.\Key\All.ahk
+Set     LWin.ahk=.\Key\LWin.ahk
+Set     RWin.ahk=.\Key\RWin.ahk
+Set     Ctrl.ahk=.\Key\Ctrl.ahk
+Set      Alt.ahk=.\Key\Alt.ahk
+Set    Shift.ahk=.\Key\Shift.ahk
+Set    Space.ahk=.\Key\Space.ahk
+Set CapsLock.ahk=.\Key\CapsLock.ahk
+Set    Input.ahk=.\Other\Input.ahk
+Set     Loop.ahk=.\Other\Loop.ahk
+Set   Dexpot.ahk=.\Other\Dexpot.ahk
+Set Software.ahk=.\Software\All.ahk
+::#endregion
 
-Set      Folder=%~dp0
-Set    Init.ahk=%Folder%Init.ahk
-Set  Config.ahk=%Folder%Config\Base.ahk
-Set  Screen.ahk=%Folder%Tool\Screen.ahk
+CD  /d  %Folder%
 
-@REM ======================================================================
-
+::#region Init Ahk Exe
 @REM                22                 -1
 @REM AutoHotkey := "D:\Xxx\AutoHotkey.exe"
 For /f "delims=" %%A In ('FindStr ".*AutoHotkey.exe" %Config.ahk%') Do Set AutoHotkey=%%A
 Set AutoHotkey=%AutoHotkey:~22,-1%
 If Not Exist %AutoHotkey% (
-    Msg %username% /time:9 配置文件错误 \ 未设置脚本执行文件路径 Config.ahk\AutoHotkey
+    Msg %username% /time:9 配置文件错误 \ 未设置脚本执行文件路径 Config\Base.ahk
     Exit
 )
 Set   AutoHotkeyDpiSoftware=%AutoHotkey:AutoHotkey.exe=AutoHotkeyDpiSoftware.exe%
 Set     AutoHotkeyDpiSystem=%AutoHotkey:AutoHotkey.exe=AutoHotkeyDpiSystem.exe%
 Set AutoHotkeyDpiSystemPlus=%AutoHotkey:AutoHotkey.exe=AutoHotkeyDpiSystemPlus.exe%
+::#endregion
 
-@REM ======================================================================
-
-CD  /d  %Folder%
-
-@REM ======================================================================
-
+::#region Run Command | Start | Stop | ForceStop | ForceStart | StopSpace
 Set  AHK=%AutoHotkey%
 Set  Command=%1
-
-@REM Command | Start | Stop | ForceStop | ForceStart | StopSpace
 
 If "%Command%"=="" (
     Set Command=Start
@@ -49,9 +57,9 @@ If "%Command%"=="" (
 
 @REM 运行游戏时结束Space.ahk
 If "%Command%"=="StopSpace" (
-    Start %AHK% .\Key\Space.ahk Stop
+    Start  %AHK%  %Space.ahk%  Stop
     Echo.
-    Echo  Stop .\Key\Space.ahk
+    Echo  Stop  %Space.ahk%
     Exit
 )
 
@@ -76,47 +84,44 @@ If "%Command%"=="Start" (
     %AutoHotkeyDpiSoftware%    %Screen.ahk%  Software
     %AutoHotkeyDpiSystem%      %Screen.ahk%  System
     %AutoHotkeyDpiSystemPlus%  %Screen.ahk%  SystemPlus
-    Start %AHK% .\Setup.ahk
+    Start  %AHK%  %Setup.ahk%
     Echo.
-    Echo  Start .\Setup.ahk
+    Echo   Start  %Setup.ahk%
 )
+::#endregion
 
-@REM ======================================================================
-
+::#region AutoHotkey Run Script
 Echo.
-Start %AHK%     .\Key\All.ahk         %Command%
-Echo  %Command% .\Key\All.ahk
+Start  %AHK%      %Keys.ahk%      %Command%
+Echo   %Command%  %Keys.ahk%
 Echo.
-Start %AHK%     .\Key\LWin.ahk        %Command%
-Echo  %Command% .\Key\LWin.ahk
-Start %AHK%     .\Key\RWin.ahk        %Command%
-Echo  %Command% .\Key\RWin.ahk
+Start  %AHK%      %LWin.ahk%      %Command%
+Echo   %Command%  %LWin.ahk%
+Start  %AHK%      %RWin.ahk%      %Command%
+Echo   %Command%  %RWin.ahk%
 Echo.
-Start %AHK%     .\Key\Ctrl.ahk        %Command%
-Echo  %Command% .\Key\Ctrl.ahk
-Start %AHK%     .\Key\Alt.ahk         %Command%
-Echo  %Command% .\Key\Alt.ahk
-Start %AHK%     .\Key\Shift.ahk       %Command%
-Echo  %Command% .\Key\Shift.ahk
+Start  %AHK%      %Ctrl.ahk%      %Command%
+Echo   %Command%  %Ctrl.ahk%
+Start  %AHK%      %Alt.ahk%       %Command%
+Echo   %Command%  %Alt.ahk%
+Start  %AHK%      %Shift.ahk%     %Command%
+Echo   %Command%  %Shift.ahk%
 Echo.
-Start %AHK%     .\Key\Space.ahk       %Command%
-Echo  %Command% .\Key\Space.ahk
-Start %AHK%     .\Key\CapsLock.ahk    %Command%
-Echo  %Command% .\Key\CapsLock.ahk
-Start %AHK%     .\Key\Other.ahk       %Command%
-Echo  %Command% .\Key\Other.ahk
+Start  %AHK%      %Space.ahk%     %Command%
+Echo   %Command%  %Space.ahk%
+Start  %AHK%      %CapsLock.ahk%  %Command%
+Echo   %Command%  %CapsLock.ahk%
 Echo.
-Start %AHK%     .\Other\Input.ahk     %Command%
-Echo  %Command% .\Other\Input.ahk
-Start %AHK%     .\Other\Loop.ahk      %Command%
-Echo  %Command% .\Other\Loop.ahk
-Start %AHK%     .\Other\Dexpot.ahk    %Command%
-Echo  %Command% .\Other\Dexpot.ahk
+Start  %AHK%      %Input.ahk%     %Command%
+Echo   %Command%  %Input.ahk%
+Start  %AHK%      %Loop.ahk%      %Command%
+Echo   %Command%  %Loop.ahk%
+Start  %AHK%      %Dexpot.ahk%    %Command%
+Echo   %Command%  %Dexpot.ahk%
 Echo.
-Start %AHK%     .\Software\All.ahk    %Command%
-Echo  %Command% .\Software\All.ahk
-
-@REM ======================================================================
+Start  %AHK%      %Software.ahk%  %Command%
+Echo   %Command%  %Software.ahk%
+::#endregion
 
 EndLocal
 
